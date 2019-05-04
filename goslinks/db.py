a8c21f82190995590611b8c44ca32c61a6578517
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-05-03 23:40:35 -0700

    Define read/write capacity units on models

--- goslinks/db.py
+++ goslinks/db.py
@@ -6,6 +6,8 @@ class UserModel(Model):
     class Meta:
         table_name = "Users"
         host = "http://localhost:8000"
+        read_capacity_units = 1
+        write_capacity_units = 1
 
     email = UnicodeAttribute(hash_key=True)
 
@@ -14,6 +16,8 @@ class LinkModel(Model):
     class Meta:
         table_name = "Links"
         host = "http://localhost:8000"
+        read_capacity_units = 1
+        write_capacity_units = 1
 
     name = UnicodeAttribute(hash_key=True)  # contains organization name and link name
     url = UnicodeAttribute()
--- goslinks/migrations/0001_initial.py
+++ goslinks/migrations/0001_initial.py
@@ -2,8 +2,8 @@ from goslinks.db import LinkModel, UserModel
 
 
 def run():
-    UserModel.create_table(read_capacity_units=1, write_capacity_units=1)
-    LinkModel.create_table(read_capacity_units=1, write_capacity_units=1)
+    UserModel.create_table()
+    LinkModel.create_table()
 
 
 if __name__ == "__main__":
