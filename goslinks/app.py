6fe91b513b3a34ad1b93b2c1c5e971a5ca3405aa
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-05-09 23:38:35 -0700

    Create app from factory

--- goslinks/app.py
+++ goslinks/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 
 from goslinks.blueprints import BLUEPRINTS
+from goslinks.config import Config
 
-app = Flask(__name__)
-app.config.from_object("goslinks.config.Config")
-for bp in BLUEPRINTS:
-    app.register_blueprint(bp)
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    for bp in BLUEPRINTS:
+        app.register_blueprint(bp)
+    return app
+
+
+app = create_app(Config)
